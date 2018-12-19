6cf736856c3951e3e9c5e765cd04b9dd6fea54fb
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-19 00:02:56 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,7 +37,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for number_of_times do 
+  for qty in number_of_times
     puts phrase
 end
 end
